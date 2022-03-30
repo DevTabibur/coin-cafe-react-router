16694a886c4aab1e98f8077a5310f38afe4135d5
DevTabibur <tobiburrohman2@gmail.com>
2022-03-30 16:42:59 +0600

    CustomLink added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Routes, Route, Link} from 'react-router-dom';
+import Header from './Components/Header/Header';
 import Homepage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <>
+    <Header/>
       <Routes>
         <Route path='/' element={<Homepage/>}></Route>
       </Routes>
