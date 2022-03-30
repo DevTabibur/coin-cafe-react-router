c6dd6f0d3d1eb62d97f96843a4b0be3042cc4ba9
DevTabibur <tobiburrohman2@gmail.com>
2022-03-31 00:42:53 +0600

    all done

---
+++ README.md
@@ -0,0 +1,9 @@
+**Live Website Link**
+
+
+
+**Technology**
+1.Spinner
+2.Tailwind css
+3.React Router (All type Link Added)
+4.Fetch Data
--- src/Components/CoinCard/CoinCard.jsx
+++ src/Components/CoinCard/CoinCard.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Spinner from '../Spinner/Spinner';
 
 const CoinCard = ({ coin }) => {
   return (
