7cab6c668c4c8769e2d5ffbee1e086d543df9cd4
DevTabibur <tobiburrohman2@gmail.com>
2022-03-30 17:06:30 +0600

    tailwindcss install success

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {Routes, Route, Link} from 'react-router-dom';
+import About from './Components/About/About';
+import Coins from './Components/Coins/Coins';
+import Contact from './Components/Contact/Contact';
 import Header from './Components/Header/Header';
 import Homepage from './Components/HomePage/HomePage';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
     <Header/>
       <Routes>
         <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/coins' element={<Coins/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </>
   );
